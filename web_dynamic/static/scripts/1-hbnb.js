c9b42b2f078af660fbd64231ff875696ba33c5a8
= <aleksifuna@gmail.com>
2024-02-09 22:56:58 +0000

    added js script that tracks checkboxes

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  const amenities = {};
+  $('li input[type=checkbox]').change(function () {
+    if (this.checked) {
+      amenities[this.dataset.id] = this.dataset.name;
+    } else {
+      delete amenities[this.dataset.id];
+    }
+    $('.amenities h4').text(Object.values(amenities).sort().join(', '));
+  });
+});
